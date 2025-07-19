0ba137b29daee1ae2c926eddb26d19fcb9e8455e
annie-mac <mcavoy.ana@gmail.com>
2025-07-19 18:59:51 +0100

    debug change

--- childcare_platform/settings.py
+++ childcare_platform/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=!#(p7iae8@cmgq)@lt4h*n$zjt_$d_fssa_d*2_fzi=ck5ey6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
     
