0fbb909439b25d5554fc0727ddb1811611f62044
annie-mac <mcavoy.ana@gmail.com>
2025-07-24 21:31:37 +0100

    add allauth and modify templates

--- childcare_platform/urls.py
+++ childcare_platform/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('providers.urls'), name='providers-urls'),  # Include the URLs from the providers app
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
 ]
