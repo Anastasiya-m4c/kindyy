289622fd645b6bd1d9dbb4151f50e17d3c83d94b
annie-mac <mcavoy.ana@gmail.com>
2025-07-25 17:03:41 +0100

    add view to allow users create profile

--- providers/views.py
+++ providers/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from .forms import PostForm
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
@@ -21,3 +24,17 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = 'kindyy/index.html'
     paginate_by = 6
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user  # Automatically set the post author
+            post.save()
+            return redirect('home')  # Redirect to your homepage or post detail
+    else:
+        form = PostForm()
+    return render(request, 'kindyy/post_form.html', {'form': form})
+
