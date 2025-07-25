fe5be85a82a3a3568d31b6fbcfd8dce3845a3751
annie-mac <mcavoy.ana@gmail.com>
2025-07-25 17:09:44 +0100

    update url path for post creation

--- providers/urls.py
+++ providers/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('create/', login_required(views.create_post), name='create_post'),  # New path for post creation
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
