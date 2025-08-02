9e837c37ae09d99765220e79de65d47b7d58af74
annie-mac <mcavoy.ana@gmail.com>
2025-08-02 11:10:47 +0100

    add search by area view and incorporate it in the nav bar

--- providers/views.py
+++ providers/views.py
@@ -3,6 +3,7 @@ from .forms import PostForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from .models import Post
+from .choices import SWANSEA_AREAS
 
 
 # Create your views here.
@@ -23,6 +24,30 @@ class PostList(generic.ListView):
     template_name = 'kindyy/index.html'
     paginate_by = 6
 
+# List of areas for the dropdown in the search form
+def post_search(request):
+    # Get the selected area from the form submission
+    selected_area = request.GET.get('area')
+
+    # Make a list of just the area names
+    areas = []
+    for area in SWANSEA_AREAS:
+        areas.append(area[0])
+
+    # If a user selected an area, filter the posts by that area
+    if selected_area:
+        posts = Post.objects.filter(area=selected_area)
+    else:
+        posts = Post.objects.all()  # no filter, show all posts
+
+    # Send the data to the template
+    return render(request, 'posts/post_search.html', {
+        'posts': posts,                 # the posts to show
+        'areas': areas,                 # list of all areas for the dropdown
+        'selected_area': selected_area  # so we know what was selected
+    })
+
+
 @login_required
 def create_post(request):
     # Create a new post
@@ -42,7 +67,6 @@ def my_posts(request):
     # Display posts created by the logged-in user
     user_posts = Post.objects.filter(author=request.user)
     return render(request, 'kindyy/my_posts.html', {'posts': user_posts})
-
 
 @login_required
 def edit_post(request, slug):
